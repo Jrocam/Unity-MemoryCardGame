76c4dc7880affdf261b26d2ecfdf1d7fb0d96af4
John Doe <johndoe@example.com>
2017-11-01 17:41:35 -0500

    first cardgame

---
+++ Assets/Scripts/CardChange.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CardChange : MonoBehaviour {
+	public SpriteRenderer sprite1, sprite3;
+	public Sprite[] sprite2;
+    public Camera camaraprincipal;
+
+
+    // Use this for initialization
+    void Start () {
+		sprite1 = GetComponent<SpriteRenderer> ();
+        StartCoroutine(TimeOut());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (camaraprincipal.GetComponent<Manager1>().PlayerChange)
+        {
+            camaraprincipal.GetComponent<Manager1>().PlayerChange = false;
+            StartCoroutine(unSec());
+            StartCoroutine(TimeOut());
+        }
+    
+	}
+	void OnMouseDown() {
+
+        if (camaraprincipal.GetComponent<Manager1>().count < 2)
+        {
+            sprite1.sprite = sprite2[0];
+            camaraprincipal.GetComponent<Manager1>().count++;
+            //PRIMERA VEZ QUE PRESIONA
+            if (camaraprincipal.GetComponent<Manager1>().count == 1)
+            {
+                camaraprincipal.GetComponent<Manager1>().primera = gameObject.tag;
+            }
+            Debug.Log(camaraprincipal.GetComponent<Manager1>().count);
+            //SEGUNDA VEZ QUE PRESIONA
+            if (camaraprincipal.GetComponent<Manager1>().count == 2)
+            {
+                //VERIFICAR CARTAS SON IGUALES
+                if (camaraprincipal.GetComponent<Manager1>().primera.Equals(gameObject.tag))
+                {
+                    if (camaraprincipal.GetComponent<Manager1>()._player1)
+                    {
+                        camaraprincipal.GetComponent<Manager1>().puntos1++;
+                    }
+                    else if (camaraprincipal.GetComponent<Manager1>()._player2)
+                    {
+                        camaraprincipal.GetComponent<Manager1>().puntos2++;
+                    }
+                }
+                camaraprincipal.GetComponent<Manager1>().PlayerChange = true;
+                camaraprincipal.GetComponent<Manager1>().count = 0;
+
+                if (camaraprincipal.GetComponent<Manager1>()._player1)
+                {
+                    Debug.Log("Jugador 2 es tu turno");
+                    camaraprincipal.GetComponent<Manager1>()._player1 = false;
+                    camaraprincipal.GetComponent<Manager1>()._player2 = true;
+                }
+                else if (camaraprincipal.GetComponent<Manager1>()._player2)
+                {
+                    Debug.Log("Jugador 1 es tu turno");
+                    camaraprincipal.GetComponent<Manager1>()._player1 = true;
+                    camaraprincipal.GetComponent<Manager1>()._player2 = false;
+                }
+            }
+        }
+
+
+    }
+
+    IEnumerator TimeOut()
+    {
+        yield return new WaitForSeconds(15f);
+        camaraprincipal.GetComponent<Manager1>().count = 0;
+        camaraprincipal.GetComponent<Manager1>().PlayerChange = true;
+        if (camaraprincipal.GetComponent<Manager1>()._player1)
+        {
+            Debug.Log("Jugador 2 es tu turno");
+            camaraprincipal.GetComponent<Manager1>()._player1 = false;
+            camaraprincipal.GetComponent<Manager1>()._player2 = true;
+        }
+        else if (camaraprincipal.GetComponent<Manager1>()._player2)
+        {
+            Debug.Log("Jugador 1 es tu turno");
+            camaraprincipal.GetComponent<Manager1>()._player1 = true;
+            camaraprincipal.GetComponent<Manager1>()._player2 = false;
+        }
+    }
+
+    IEnumerator unSec()
+    {
+        yield return new WaitForSeconds(1f);
+        camaraprincipal.GetComponent<Manager1>().voltearCartas();
+    }
+}
---
+++ Assets/Scripts/Manager1.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager1 : MonoBehaviour {
+
+    public Text Puntos1;
+    public Text Puntos2;
+
+    public int count = 0;
+    public bool PlayerChange = false;
+    public bool _player1 = true;
+    public bool _player2 = false;
+    public GameObject[] cartas;
+    public Sprite ogSprite;
+    public string primera;
+    public string segunda;
+    public int puntos1=0;
+    public int puntos2=0;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Puntos1.text = "Puntos1= " + puntos1;
+        Puntos2.text = "Puntos2= " + puntos2;
+    }
+    public void voltearCartas()
+    {
+        for (int i = 0; i < cartas.Length; i++)
+        {
+            cartas[i].gameObject.GetComponent<SpriteRenderer>().sprite = ogSprite;
+        }
+    }
+}
